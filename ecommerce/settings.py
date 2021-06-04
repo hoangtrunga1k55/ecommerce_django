7297ac084ff010755ffdf54ef9fb06b364cafa92
hoangtrung <hoangtrunga1k55>
2021-06-04 10:28:01 +0700

    sua giao dien

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'tmdt2',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
